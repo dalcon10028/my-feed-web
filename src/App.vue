14cba92ba72328df1cad772aecc7197182e63772
dalcon10028 <dalcon10280@gmail.com>
2022-04-08 21:40:14 +0900

    chore: TypeScript 세팅

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
-<script setup>
-import { ref } from 'vue'
+<script setup lang="ts">
+import { reactive } from 'vue'
 
-let counter = ref(0)
+interface Counter {
+  me: number
+}
+
+let counter = reactive<Counter>({
+  me: 0
+})
 
 setInterval(() => {
-  counter.value++
+  counter.me++;
 }, 1000)
 </script>
 
@@ -13,10 +19,10 @@ setInterval(() => {
     <header class="bg-white shadow" v-if="$route.meta.title">
       <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
         <h1
-          @click="counter = 0"
+          @click="counter.me = 0"
           class="text-3xl font-bold leading-tight text-gray-900"
         >
-          {{ $route.meta.title }} / {{ counter }}
+          {{ $route.meta.title }} / {{ counter.me }}
         </h1>
       </div>
     </header>
