97914ada1904f5b924ba78bccd2a13050b401e2e
dalcon10028 <dalcon10280@gmail.com>
2022-05-07 00:51:16 +0900

    refactor: vuex typescript 적용

--- src/store/user/index.ts
+++ src/store/user/index.ts
@@ -1,53 +1,65 @@
 import { fetchUserProfile } from '@/apis/users';
 import instance from '@/apis/instance';
 import { FETCH_JWT_TOKEN, FETCH_USER_PROFILE } from './mutation-types';
+import type { ActionContext, ActionTree, GetterTree, Module, MutationTree } from 'vuex';
+import axios from 'axios';
 
 interface User {
-  id?: number;
-  username?: string;
-  nickname?: string;
-  provider?: string;
+  id: number;
+  username: string;
+  nickname: string;
+  provider: string;
   thumnail: string;
-  created_at?: Date;
-  updated_at?: Date;
+  created_at: Date;
+  updated_at: Date;
 }
 
-export default {
-  namespaced: true,
-  state: () => ({
-    user: {},
-    jwtToken: localStorage.getItem('myfeed-token') ?? ''
-  }),
-  getters: {
-    isLogin(state: { user: any }) {
-      return !!state.user.id;
-    }
+export class State {
+  public user: User | object = {};
+  public jwtToken: string = localStorage.getItem('myfeed-token') ?? '';
+}
+
+const getters: GetterTree<State, unknown> = {
+  isLogin(state: State) {
+    return 'id' in state.user ? !!state.user.id : false;
+  }
+};
+
+const mutations: MutationTree<State> = {
+  [FETCH_JWT_TOKEN](state: { jwtToken: string }, jwtToken: string) {
+    state.jwtToken = jwtToken;
+    localStorage.setItem('myfeed-token', jwtToken);
+    instance.interceptors.request.use((config: any) => {
+      config.headers.Authorization = `Bearer ${jwtToken}`;
+      return config;
+    });
   },
-  mutations: {
-    [FETCH_JWT_TOKEN](state: { jwtToken: string }, jwtToken: string) {
-      state.jwtToken = jwtToken;
-      localStorage.setItem('myfeed-token', jwtToken);
-      instance.interceptors.request.use((config: any) => {
-        config.headers.Authorization = `Bearer ${jwtToken}`;
-        return config;
-      });
-    },
-    [FETCH_USER_PROFILE](state: any, userData: User) {
-      state.user = userData;
-    }
-  },
-  actions: {
-    async fetchUserProfile({ commit }: any) {
-      /* 프로필 갱신 */
-      try {
-        const { data } = await fetchUserProfile();
-        commit(FETCH_USER_PROFILE, data);
-      } catch (error: any) {
+  [FETCH_USER_PROFILE](state: State, userData: User) {
+    state.user = userData;
+  }
+};
+
+const actions: ActionTree<State, unknown> = {
+  async fetchUserProfile({ commit }: ActionContext<State, unknown>) {
+    /* 프로필 갱신 */
+    try {
+      const { data } = await fetchUserProfile();
+      commit(FETCH_USER_PROFILE, data);
+    } catch (error) {
+      if (axios.isAxiosError(error) && error.request.statusText === 'Unauthorized')
         /* 토큰이 유효하지 않을 경우 삭제 */
-        if (error.request.statusText === 'Unauthorized') {
-          localStorage.removeItem('myfeed-token');
-        }
-      }
+        localStorage.removeItem('myfeed-token');
+      else alert(error);
     }
   }
 };
+
+const UserModule: Module<State, unknown> = {
+  namespaced: true,
+  state: new State(),
+  getters,
+  mutations,
+  actions
+};
+
+export default UserModule;
