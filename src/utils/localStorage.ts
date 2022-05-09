bc8bd5200fd079d6b6e9615ab3c8487109780a1f
dalcon10028 <dalcon10280@gmail.com>
2022-05-09 22:39:06 +0900

    feat: 토큰 제거 메소드 추가

--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -2,11 +2,14 @@ export class TokenStorage {
   static KEY_NAME = 'myfeed-token';
 
   static setToken(token: string) {
-    console.log(`token: ${token}, keyName: ${this.KEY_NAME}`);
     localStorage.setItem(this.KEY_NAME, token);
   }
 
   static getToken(): string {
     return localStorage.getItem(this.KEY_NAME) ?? '';
+  }
+
+  static removeToken(): void {
+    localStorage.removeItem(this.KEY_NAME);
   }
 }
